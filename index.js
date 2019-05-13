5d7c8eb50a4e50f6476c8957f9e16a12fbb64b5b
Gia Edgington <gia.edgington@gmail.com>
2019-05-13 23:34:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var BeatlesPlay = []
+    for(let i = 0; j < musicians.length; j++){
+      BeatlesPlay.push(musicians[i] + " plays " + instruments[i]);
+      i++
+    }
+  return BeatlesPlay
+}
+
+function johnLennonFacts(array){
+  return 
+}
